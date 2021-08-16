ca4ef8e1ad98201b921a3d633a094bd03af0bc89
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-08-16 10:32:02 -0400

    Add files via upload

---
+++ Assets/Scripts/DragLine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DragLine : MonoBehaviour
+{
+    // Cached References
+    LineRenderer _lineRenderer;
+
+    Bird _bird;
+
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _bird = FindObjectOfType<Bird>();
+
+        Vector3 lineZeroPosition = new Vector3(
+            _bird.transform.position.x, 
+            _bird.transform.position.y, 
+            -0.1f);
+
+        _lineRenderer.SetPosition(0, lineZeroPosition);
+    }
+
+    void Update()
+    {
+        if (_bird.IsDragging)
+        {
+            _lineRenderer.enabled = true;
+            _lineRenderer.SetPosition(1, _bird.transform.position);
+        }
+        else
+            _lineRenderer.enabled = false;
+    }
+}
